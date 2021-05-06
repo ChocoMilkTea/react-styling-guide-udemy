23658787954d6270b19962b1cf845900b75e3736
ChocoMilkTea <hongjun87@gmail.com>
2021-05-07 01:34:30 +0800

    include all logic without styling

--- src/App.js
+++ src/App.js
@@ -18,13 +18,26 @@ const App = () => {
     });
   }
 
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    })
+  }
+
+  let content = <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+
+  if (courseGoals.length > 0) {
+    content = <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+  }
+
   return (
     <div>
       <section id='goal-form'>
         <CourseInput onAddGoal={addGoalHandler} />
       </section>
       <section id='goals'>
-        <CourseGoalList items={courseGoals} />
+        {content}
       </section>
     </div>
   );
