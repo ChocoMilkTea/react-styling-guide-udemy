e28c12f3db357f6c3f2d2502fd607524be7d5ad6
ChocoMilkTea <hongjun87@gmail.com>
2021-05-09 18:22:09 +0800

    setting inline style

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -4,13 +4,24 @@ import './CourseInput.css';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
     setEnteredValue('');
   };
@@ -18,9 +29,14 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
-        <label>Course Goal</label>
-        <input type='text'
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input
+          type='text'
           value={enteredValue}
+          style={{
+            borderColor: !isValid ? 'red' : '#ccc',
+            background: !isValid? 'salmon' : 'transparent'
+          }}
           onChange={goalInputChangeHandler}
         />
       </div>
